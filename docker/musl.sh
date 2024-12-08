aa6defbbf5d7ee8fea1def7bac629225d0c820be
kiki-kanri <a470666@gmail.com>
2024-12-08 17:36:54 +0800

    feat: update musl-cross-make, gcc and binutils versions during docker build for ubuntu 24.04 or higher

--- docker/musl.sh
+++ docker/musl.sh
@@ -15,14 +15,22 @@ hide_output() {
     " ERR
     bash -c 'while true; do sleep 30; echo $(date) - building ...; done' &
     PING_LOOP_PID=$!
-    "${@}" &> /tmp/build.log
+    "${@}" &>/tmp/build.log
     trap - ERR
     kill "${PING_LOOP_PID}"
     set -x
 }
 
 main() {
+    local binutils_version=2.33.1
+    local gcc_version=9.2.0
+    local musl_version=1.2.3
     local version=fe91582
+
+    if_ubuntu_ge 24.04 binutils_version=2.43.1
+    if_ubuntu_ge 24.04 gcc_version=13.3.0
+    if_ubuntu_ge 24.04 musl_version=1.2.5
+    if_ubuntu_ge 24.04 version=fd6be58
 
     install_packages ca-certificates curl build-essential
 
@@ -37,6 +45,17 @@ main() {
     local linux_headers_site=https://ci-mirrors.rust-lang.org/rustc/sabotage-linux-tarballs
     local linux_ver=headers-4.19.88
 
+    # Ensure sources directory exists
+    mkdir -p ./sources
+
+    # Download gcc
+    download_gcc $gcc_version gz
+    mv "./gcc-$gcc_version.tar.gz" ./sources/
+
+    # Download binutils
+    download_binutils $binutils_version gz
+    mv "./binutils-$binutils_version.tar.gz" ./sources/
+
     # alpine GCC is built with `--enable-default-pie`, so we want to
     # ensure we use that. we want support for shared runtimes except for
     # libstdc++, however, the only way to do that is to simply remove
@@ -44,9 +63,9 @@ main() {
     # linked, so our behavior has maximum portability, and is consistent
     # with popular musl distros.
     hide_output make install "-j$(nproc)" \
-        GCC_VER=9.2.0 \
-        MUSL_VER=1.2.3 \
-        BINUTILS_VER=2.33.1 \
+        GCC_VER=$gcc_version \
+        MUSL_VER=$musl_version \
+        BINUTILS_VER=$binutils_version \
         DL_CMD='curl --retry 3 -sSfL -C - -o' \
         LINUX_HEADERS_SITE="${linux_headers_site}" \
         LINUX_VER="${linux_ver}" \
