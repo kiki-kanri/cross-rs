c318961a069143120ec5aad66458fed5e8464128
kiki-kanri <a470666@gmail.com>
2024-12-08 17:27:14 +0800

    feat: replace repository URL during docker build for Ubuntu systems if `/etc/apt/sources.list.d/ubuntu.sources` is detected

--- docker/common.sh
+++ docker/common.sh
@@ -10,9 +10,12 @@ set -euo pipefail
 # This is important if you enable additional architectures so you can install libraries to cross-compile against.
 # Look for 'dpkg --add-architecture' in the README for more details.
 if grep -i ubuntu /etc/os-release >/dev/null; then
-    sed 's/http:\/\/\(.*\).ubuntu.com\/ubuntu\//[arch-=amd64,i386] http:\/\/ports.ubuntu.com\/ubuntu-ports\//g' /etc/apt/sources.list > /etc/apt/sources.list.d/ports.list
+    sed 's/http:\/\/\(.*\).ubuntu.com\/ubuntu\//[arch-=amd64,i386] http:\/\/ports.ubuntu.com\/ubuntu-ports\//g' /etc/apt/sources.list >/etc/apt/sources.list.d/ports.list
     sed -i 's/http:\/\/\(.*\).ubuntu.com\/ubuntu\//[arch=amd64,i386] http:\/\/\1.archive.ubuntu.com\/ubuntu\//g' /etc/apt/sources.list
 fi
+
+# Change sources if /etc/apt/sources.list.d/ubuntu.sources is detected.
+[ -f /etc/apt/sources.list.d/ubuntu.sources ] && sed -i 's|http://.*archive.ubuntu.com|http://free.nchc.org.tw|g; s|http://.*security.ubuntu.com|http://free.nchc.org.tw|g' /etc/apt/sources.list.d/ubuntu.sources
 
 install_packages \
     autoconf \
