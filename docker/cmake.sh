048e4421ebe573a08f39040c48a99823353e1eed
kiki-kanri <a470666@gmail.com>
2025-09-06 18:09:24 +0800

    chore: update docker cmake script

--- docker/cmake.sh
+++ docker/cmake.sh
@@ -23,19 +23,19 @@ main() {
     narch="$(dpkg --print-architecture)"
 
     case "${narch}" in
-        amd64)
-            cmake_arch="linux-x86_64"
-            cmake_sha256="da2a9b18c3bfb136917fa1a579aa5316b01c1d6c111043d03f18877ff05bda30"
-            ;;
-        arm64)
-            cmake_arch="linux-aarch64"
-            cmake_sha256="86122bdfd030208aa36705ef421a218ccec52a14368020b2d67043af5e45490b"
-            ;;
-        *)
-            echo "Unsupported architecture: ${narch}" 1>&2
-            exit 1
-            ;;
-     esac
+    amd64)
+        cmake_arch="linux-x86_64"
+        cmake_sha256="33ba237f2850a82d5c71c1d41803ab832a9a7aac7d99fa6e48bbe5bb4d66653c"
+        ;;
+    arm64)
+        cmake_arch="linux-aarch64"
+        cmake_sha256="86122bdfd030208aa36705ef421a218ccec52a14368020b2d67043af5e45490b"
+        ;;
+    *)
+        echo "Unsupported architecture: ${narch}" 1>&2
+        exit 1
+        ;;
+    esac
 
     curl --retry 3 -sSfL "https://github.com/Kitware/CMake/releases/download/v${version}/cmake-${version}-${cmake_arch}.sh" -o cmake.sh
     sha256sum --check <<<"${cmake_sha256}  cmake.sh"
